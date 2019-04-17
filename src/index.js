c291f04191a8a576c7205a135268a4b4c2e0645f
barrick12 <dylan.ntk@gmail.com>
2019-04-16 21:10:07 -0400

    Added Todo example code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 const App = () => (
 	<div>
 		<h1>Hello world!!</h1>
 	</div>
 )
 ReactDOM.render(<App/>, document.getElementById('root'));
+
+// class TodoApp extends React.Component {
+//   constructor(props) {
+//     super(props);
+//
+// 	this.state = {
+// 		items: [],
+// 		text: ''
+// 	};
+//
+// 	this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <h3>TODO</h3>
+//
+//         <TodoList items={this.state.items} />
+//
+// 		<form onSubmit={this.handleSubmit}>
+//           <label htmlFor="new-todo">
+//             What needs to be done?<br/>
+//           </label>
+//           <input
+//             id="new-todo"
+//             onChange={this.handleChange}
+//             value={this.state.text}
+//           />
+//           <button type="submit">
+//             Add #{this.state.items.length + 1}
+//           </button>
+//         </form>
+//
+//       </div>
+//     );
+//   }
+//
+//   handleChange(e) {
+//     this.setState({ text: e.target.value });
+//   }
+//
+//   handleSubmit(e) {
+//     e.preventDefault();
+//
+//     if (!this.state.text.length) {
+//       return;
+//     }
+//
+//     const newItem = {
+//       text: this.state.text,
+//       id: Date.now()
+//     };
+//
+//     this.setState(state => ({
+//       items: state.items.concat(newItem),
+//       text: ''
+//     }));
+//   }
+// }
+//
+// class TodoList extends React.Component {
+//
+//   render() {
+//     return (
+//       <ul>
+//         {this.props.items.map(item => (
+//           <li key={item.id}>{item.text}</li>
+//         ))}
+//       </ul>
+//     );
+//   }
+//
+// }
+//
+// ReactDOM.render(
+//   <TodoApp />,
+//   document.getElementById('root')
+// );
